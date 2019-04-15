3b18a506f45770ceea63d2b903ec773fbbb89c65
shanglicheng <38649990+shanglicheng@users.noreply.github.com>
2019-04-15 10:50:18 +0800

    Update RandomPooling.R

--- RandomPooling.R
+++ RandomPooling.R
@@ -1,4 +1,3 @@
-
 ########################
 # sn: The duplicate times for each pooling
 # matrix1: the read counts matrix of group 1, the rows represent genes and the columns represent samples
@@ -6,15 +5,17 @@
 #group1Inf: the vector of the sample library.
 #group2Inf: the vector of the sample library.
 #outDir: The output directory.
-myRandomPooling = function(matrix1, matrix2, sn, group1Inf, group2Inf, outDir){
+#parallel_TF: to parallelly run DESeq2
+#ncores: the number of cores used for running DESeq2
+myRandomPooling = function(matrix1, matrix2, sn, group1Inf, group2Inf, outDir, parallel_TF, ncores){
   
   difgeneNum.df = c(0, 0)
-  difgeneNum.SNc = c(0, 0)
-  difgeneNum.VTA = c(0, 0)
-  difgene.SNc.df = rep("x", nrow(matrix1))
-  difgene.VTA.df = rep("x", nrow(matrix1))
-  difgene.SNc.p = rep(2, nrow(matrix1))
-  difgene.VTA.p = rep(2, nrow(matrix1))
+  difgeneNum.Group1 = c(0, 0)
+  difgeneNum.Group2 = c(0, 0)
+  difgene.Group1.df = rep("x", nrow(matrix1))
+  difgene.Group2.df = rep("x", nrow(matrix1))
+  difgene.Group1.p = rep(2, nrow(matrix1))
+  difgene.Group2.p = rep(2, nrow(matrix1))
   
   poolingVec = c(3:min(col(matrix1), col(matrix2)))
   
@@ -24,7 +25,7 @@ myRandomPooling = function(matrix1, matrix2, sn, group1Inf, group2Inf, outDir){
       index.spl = sample(1:ncol(matrix1),sampleNum)
       matrix1.tmp = matrix1[,index.spl]; 
       group1.tmp = data.frame(Type = c(group1Inf)[index.spl],Time = c(group1Inf)[index.spl])
-
+      
       matrix2.tmp = matrix2[,index.spl]; 
       group2.tmp = data.frame(Type = c(group2Inf)[index.spl],Time = c(group2Inf)[index.spl])
       
@@ -39,8 +40,8 @@ myRandomPooling = function(matrix1, matrix2, sn, group1Inf, group2Inf, outDir){
       
       #DESeq2
       metaDataForD = data.frame(type = sampleInf_r1)
-
-      res = myDESeq2(reads = read_r1.qc, GroupDf = metaDataForD)
+      
+      res = myDESeq2(reads = read_r1.qc, GroupDf = metaDataForD, parallel_TF = parallel_TF, ncores = ncores)
       
       #resInf = res@elementMetadata
       
@@ -52,60 +53,74 @@ myRandomPooling = function(matrix1, matrix2, sn, group1Inf, group2Inf, outDir){
       
       difgene = difgene[difgene$padj<0.05,]
       
-      difgene.SNc = difgene[difgene$log2FoldChange<0,]
-      difgene.VTA = difgene[difgene$log2FoldChange>0,]
+      difgene.Group1 = difgene[difgene$log2FoldChange<0,]
+      difgene.Group2 = difgene[difgene$log2FoldChange>0,]
       
       difgeneNum.df = rbind(difgeneNum.df, c(sampleNum, nrow(difgene)))
-      difgeneNum.SNc = rbind(difgeneNum.SNc, c(sampleNum, nrow(difgene.SNc)))
-      difgeneNum.VTA = rbind(difgeneNum.VTA, c(sampleNum, nrow(difgene.VTA)))
-      difgene.SNc.df = cbind(difgene.SNc.df, c(rownames(difgene.SNc), rep("x", nrow(matrix1)-nrow(difgene.SNc))))
-      difgene.VTA.df = cbind(difgene.VTA.df, c(rownames(difgene.VTA), rep("x", nrow(matrix1)-nrow(difgene.VTA))))
-      difgene.SNc.p = cbind(difgene.SNc.p, c((difgene.SNc$padj), rep(2, nrow(matrix1)-nrow(difgene.SNc))))
-      difgene.VTA.p = cbind(difgene.VTA.p, c((difgene.VTA$padj), rep(2, nrow(matrix1)-nrow(difgene.VTA))))
+      difgeneNum.Group1 = rbind(difgeneNum.Group1, c(sampleNum, nrow(difgene.Group1)))
+      difgeneNum.Group2 = rbind(difgeneNum.Group2, c(sampleNum, nrow(difgene.Group2)))
+      difgene.Group1.df = cbind(difgene.Group1.df, c(rownames(difgene.Group1), rep("x", nrow(matrix1)-nrow(difgene.Group1))))
+      difgene.Group2.df = cbind(difgene.Group2.df, c(rownames(difgene.Group2), rep("x", nrow(matrix1)-nrow(difgene.Group2))))
+      difgene.Group1.p = cbind(difgene.Group1.p, c((difgene.Group1$padj), rep(2, nrow(matrix1)-nrow(difgene.Group1))))
+      difgene.Group2.p = cbind(difgene.Group2.p, c((difgene.Group2$padj), rep(2, nrow(matrix1)-nrow(difgene.Group2))))
     }
   }
   
-  #SNc DEGs
-  deg.SNc = difgene.SNc.df
-  deg.SNc = c(as.matrix(deg.SNc))
-  deg.SNc = deg.SNc[!deg.SNc == "x"]
-  write.table(x = table(deg.SNc), file = paste(outDir, "Group1.DEGs.Frequency.txt"), sep = "\t")
+  #Group1 DEGs
+  deg.Group1 = difgene.Group1.df
+  deg.Group1 = c(as.matrix(deg.Group1))
+  deg.Group1 = deg.Group1[!deg.Group1 == "x"]
+  write.table(x = table(deg.Group1), file = paste(outDir, "Group1.DEGs.Frequency.txt"), sep = "\t")
   
-  #VTA DEGs
-  deg.VTA = difgene.VTA.df
-  deg.VTA = c(as.matrix(deg.VTA))
-  deg.VTA = deg.VTA[!deg.VTA == "x"]
-  write.table(x = table(deg.VTA), file = paste(outDir, "Group2.DEGs.Frequency.txt"), sep = "\t")
+  #Group2 DEGs
+  deg.Group2 = difgene.Group2.df
+  deg.Group2 = c(as.matrix(deg.Group2))
+  deg.Group2 = deg.Group2[!deg.Group2 == "x"]
+  write.table(x = table(deg.Group2), file = paste(outDir, "Group2.DEGs.Frequency.txt"), sep = "\t")
   
 }
 
 ######################
 myGeneExpFilter = function(rpkm, rpkmCutOff, expRatio) {
-  rpkm_l1_order = rpkm
   ExpGeneRatio = expRatio
   gene_expression_cell_num_1 <- NULL
-  for (i in 1:nrow(rpkm_l1_order)) {
-    gene_expression_cell_num_1[i] <- sum(rpkm_l1_order[i,]>rpkmCutOff)
+  for (i in 1:nrow(rpkm)) {
+    gene_expression_cell_num_1[i] <- sum(rpkm[i,]>rpkmCutOff)
   }
-  gene_expression_cell_num_1_f <- (gene_expression_cell_num_1 >= ncol(rpkm_l1_order)*ExpGeneRatio)#second filtering row for gene
+  gene_expression_cell_num_1_f <- (gene_expression_cell_num_1 >= ncol(rpkm)*ExpGeneRatio)#second filtering row for gene
   #table(gene_expression_cell_num_1_f)
-  rpkm_l1_order <- rpkm_l1_order[gene_expression_cell_num_1_f,]
-  return(rpkm_l1_order)
+  rpkm <- rpkm[gene_expression_cell_num_1_f,]
+  return(rpkm)
 }
 
 ######################
 #DESeq2
-myDESeq2 = function(reads, GroupDf) {
+myDESeq2 = function(reads, GroupDf, parallel_TF, ncores) {
   
-  library("DESeq2")
-  cts = reads
-  coldata = GroupDf
-  colnames(coldata) = c("Group")
-  dds <- DESeqDataSetFromMatrix(countData = cts,
-                                colData = coldata,
-                                design = ~ Group)
-  #dds
-  dds = DESeq(dds)
-  res = results(dds)
+  if(parallel_TF) {
+    library("DESeq2")
+    cts = reads
+    coldata = GroupDf
+    colnames(coldata) = c("Group")
+    dds <- DESeqDataSetFromMatrix(countData = cts,
+                                  colData = coldata,
+                                  design = ~ Group)
+    #dds
+    library(BiocParallel)
+    register(MulticoreParam(ncores))
+    dds = DESeq(dds, parallel = parallel_TF)
+    res = results(dds)
+  } else {
+    library("DESeq2")
+    cts = reads
+    coldata = GroupDf
+    colnames(coldata) = c("Group")
+    dds <- DESeqDataSetFromMatrix(countData = cts,
+                                  colData = coldata,
+                                  design = ~ Group)
+    #dds
+    dds = DESeq(dds)
+    res = results(dds)
+  }
   return(res)
 }
